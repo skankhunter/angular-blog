84c720ab3926f4a49b385156ad2d5d55a7d30155
Max <intelmax3@gmail.com>
2019-12-18 00:02:33 +0300

    Добавлены новые кнопки

--- src/app/admin/shared/services/auth.guard.ts
+++ src/app/admin/shared/services/auth.guard.ts
@@ -6,24 +6,21 @@ import {AuthService} from './auth.service';
 @Injectable()
 
 export class AuthGuard implements CanActivate {
-  constructor(private auth: AuthService, private router: Router) {
-
-  }
-
+  constructor(private auth: AuthService, private router: Router) {}
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
    if (this.auth.isAuthenticated()) {
-     return true
+     return true;
    } else {
      this.auth.logout();
      this.router.navigate(['/admin', 'login'], {
        queryParams: {
          loginAgain: true
        }
-     })
+     });
    }
   }
 
